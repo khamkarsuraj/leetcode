c58fbabf4704cdf8991a6c479546cae3cc6ed98a
Suraj Khamkar <112414255+surajkhamkar02@users.noreply.github.com>
2023-01-28 23:41:51 -0500

    27_Remove_Element

---
+++ Remove_Element.py
@@ -0,0 +1,20 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        start = 0
+        end = len(nums)-1
+        count = 0
+        i=0
+
+        if len(nums) == 0:
+            return 0
+
+        while(i < len(nums) and nums[i] != -1):
+            if nums[i] == val:
+                nums[i] = nums[end]
+                nums[end] = -1
+                end-=1
+            else:
+                i+=1
+                count += 1
+
+        return count
