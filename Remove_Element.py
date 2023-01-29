039070674ab81202bd4f18d7f58a8aab03210043
Suraj Khamkar <112414255+surajkhamkar02@users.noreply.github.com>
2023-01-29 00:12:14 -0500

    Update Remove_Element.py

--- Remove_Element.py
+++ Remove_Element.py
@@ -18,3 +18,12 @@ class Solution:
                 count += 1
 
         return count
+        
+        '''count = 0
+
+        for i in nums:
+            if i != val:
+                nums[count] = i
+                count+=1
+
+        return count'''
