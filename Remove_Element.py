6a6bb3f86ed706ab91093158b60677a2cc4633db
Suraj Khamkar <112414255+surajkhamkar02@users.noreply.github.com>
2023-01-29 00:18:29 -0500

    27_Remove_Element

--- Remove_Element.py
+++ Remove_Element.py
@@ -3,25 +3,26 @@ class Solution:
         start = 0
         end = len(nums)-1
         count = 0
-        i=0
 
         if len(nums) == 0:
             return 0
 
-        while(i < len(nums) and nums[i] != -1):
-            if nums[i] == val:
-                nums[i] = nums[end]
+        while(start < len(nums) and nums[start] != -1):
+            if nums[start] == val:
+                nums[start] = nums[end]
                 nums[end] = -1
                 end-=1
             else:
-                i+=1
-                count += 1
+                start+=1
+                count+= 1
 
         return count
         
         '''count = 0
+
         for i in nums:
             if i != val:
                 nums[count] = i
                 count+=1
+
         return count'''
