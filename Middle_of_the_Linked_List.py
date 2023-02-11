b9039aaed875bebb6f7ab27e82a55e8842624ed0
Suraj Khamkar <112414255+khamkarsuraj@users.noreply.github.com>
2023-02-11 13:06:25 -0500

    876_Middle_of_the_Linked_List

--- Middle_of_the_Linked_List.py
+++ Middle_of_the_Linked_List.py
@@ -5,6 +5,7 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        '''
         # Approach 1
         # Idea is to find length of the list and return list from middle node
         if not head:
@@ -23,3 +24,19 @@ class Solution:
             len-=1
             temp = temp.next
         return temp
+        '''
+
+        # Approach 2
+        # Traverse nodes with two pointers, slow will visit each node fast will goes alternate
+        # The time fast will reach at end, slow will be at middle node
+        slow = fast = head
+
+        while fast:
+            fast = fast.next
+            if fast:
+                fast = fast.next
+            else:
+                break
+            slow = slow.next
+        
+        return slow
