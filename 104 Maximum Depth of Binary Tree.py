f1c0221b1600e7a59532c426f3eabe2588fb3de4
Suraj Khamkar <112414255+khamkarsuraj@users.noreply.github.com>
2023-10-25 18:01:36 -0400

    Recursion

---
+++ 104 Maximum Depth of Binary Tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    #Recursion
+    #Time: O(n)
+    #Space: O(1)
+    def depth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        return max(self.depth(root.left), self.depth(root.right))+1
+
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        return self.depth(root)
