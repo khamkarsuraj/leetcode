e5f98a80acff8288b2d3acb8282ed480020d5e21
Suraj Khamkar <112414255+khamkarsuraj@users.noreply.github.com>
2024-04-11 09:43:38 -0400

    One Pointer

---
+++ 121 Best Time to Buy and Sell Stock.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # One pointer
+        # Time: O(n)
+        # Space: O(1)
+        profit, buy = 0, float('inf')
+        
+        for p in prices:
+            if p < buy:
+                buy = p
+            elif profit < p - buy:
+                profit = p - buy
+        
+        return profit
+
+        '''
+        # Sliding window | Two pointers
+        # Time: O(n)
+        # Space: O(1)
+        while sell < len(prices):
+            if prices[sell] - prices[buy] < 0:
+                buy += 1
+                sell = buy + 1
+            else:
+                profit = max(profit, prices[sell] - prices[buy])
+                sell += 1
+            
+        return profit
+        '''
