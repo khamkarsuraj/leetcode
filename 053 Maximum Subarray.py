581bf028986979a43fd596fb4710bbd5ce831cc5
Suraj Khamkar <112414255+khamkarsuraj@users.noreply.github.com>
2024-04-15 17:41:17 -0400

    Kadane's Algorithm

--- 053 Maximum Subarray.py
+++ 053 Maximum Subarray.py
@@ -1,11 +1,20 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # Approach 1: Iterate through list
-        # Time Complexity: O(n) | Space Complexity: O(1)
-        num = total = nums[0]        
-
-        for i in range(1, len(nums)):
-            num = max(nums[i], nums[i] + num)
-            total = max(num, total)
+        # Kadane's Algorithm
+        # Time: O(n)
+        # Space: O(1)
         
-        return total
+        sum = 0
+        # First element always because
+        # there has to atleast one element in subarray
+        maximum = nums[0]
+
+        for i in nums:
+            sum = sum + i
+            maximum = max(maximum, sum)
+            # If there is number causes decrement in sum
+            # start new sub-array
+            if sum < 0:
+                sum = 0
+        
+        return maximum
