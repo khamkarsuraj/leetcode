b87dfa08c7e9ba62b401de11e2b01f24e04df91a
Suraj Khamkar <112414255+khamkarsuraj@users.noreply.github.com>
2024-10-08 18:36:53 -0400

    Using Dictionary

--- 0049 Group Anagrams.py
+++ 0049 Group Anagrams.py
@@ -1,8 +1,28 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Using Dictionary
-        # Time O(n)
+        # Using Array
+        # Time O(mn)
         # Space O(n)
+        # n number of strings
+        # m average length of string
+        ans = defaultdict(list)
+
+        for s in strs:
+            charArray = [0] * 26
+
+            for char in s:
+                charArray[ord(char) - ord('a')] += 1
+
+            ans[tuple(charArray)].append(s)
+
+        return ans.values()
+
+        # Using Dictionary
+        # Time O(nklogk)
+        # Space O(nk)
+        # n number of strings
+        # k average length of string
+        '''
         ans = {}
         for i in range(0, len(strs)):
             sorted_key = ''.join(sorted(strs[i]))
@@ -12,3 +32,4 @@ class Solution:
                 ans[sorted_key].append(strs[i])
 
         return ans.values()
+        '''
