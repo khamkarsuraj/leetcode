9789389a6439e24aeba20e42581e564b2ffc639f
Suraj Khamkar <112414255+khamkarsuraj@users.noreply.github.com>
2024-10-10 13:21:06 -0400

    0238 Product of Array Except Self.py

--- 0238 Product of Array Except Self.py
+++ 0238 Product of Array Except Self.py
@@ -1,17 +1,36 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # Refer Java solution for explanation
-        pro = 1
-        res = [0] * len(nums)
-        
+        # Prefix, Sufix Product
+        # Time O(n)
+        # Space (1)
+        output = [0] * len(nums)
+        product = 1
+
+        # Calculate prefix product first
         for i in range(0, len(nums)):
-            res[i] = pro
-            pro = res[i] * nums[i]
+            output[i] = product
+            product = product * nums[i]
         
-        pro = 1
+        product = 1
+        # Now as we already have prefix calculated
+        # Calculate sufix and add it to output array
         for i in range(len(nums)-1, -1, -1):
-            res[i] = pro * res[i]
-            pro = pro * nums[i]
+            output[i] = output[i] * product
+            product = product * nums[i]
         
-        return res
-            
+        return output
+
+        # Brute Force Method - TLE
+        # Time O(n^2)
+        # Space O(n)
+        '''
+        output = [0] * len(nums)
+        for i in range(0, len(nums)):
+            prod = 1
+            for j in range(0, len(nums)):
+                if i != j:
+                    prod = prod * nums[j]
+            output[i] = prod
+
+        return output
+        '''
