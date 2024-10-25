85fa2e58000c615c9891e13179c35f084fe492cd
Suraj Khamkar <112414255+khamkarsuraj@users.noreply.github.com>
2024-10-25 19:50:02 -0400

    Two Pointers

--- 0125 Valid Palindrome.py
+++ 0125 Valid Palindrome.py
@@ -1,19 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        # Using Two Pointers
+        # Time O(n)
+        # Space O(1)
         left, right = 0, len(s)-1
         s = s.lower()
 
         while left < right:
-            while left < len(s)-1 and (ord(s[left]) < 97 or ord(s[left]) > 122) and (ord(s[left]) < 48 or ord(s[left]) > 57):
+            while left < right and not s[left].isalnum():
                 left+=1
 
-            while right >= 0 and (ord(s[right]) < 97 or ord(s[right]) > 122) and (ord(s[right]) < 48 or ord(s[right]) > 57):
+            while left < right and not s[right].isalnum():
                 right-=1
             
-            if ord(s[left]) != ord(s[right]):
+            if s[left] != s[right]:
                 return False
-            else:
-                left+=1
-                right-=1
+
+            left+=1
+            right-=1
 
         return True
