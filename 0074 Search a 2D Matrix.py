a510d8e0ee9da3c1e304d971fda61a94f0bb2b40
Suraj Khamkar <112414255+khamkarsuraj@users.noreply.github.com>
2025-01-10 14:48:51 -0500

    Binary Search

--- 0074 Search a 2D Matrix.py
+++ 0074 Search a 2D Matrix.py
@@ -1,5 +1,65 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # time logmn
+        # space 1
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+    
+        while left <= right:
+            mid = (left + right) // 2
+            mid_val = matrix[mid // n][mid % n]  # Convert 1D index to 2D row and column
+    
+            if mid_val == target:
+                return True
+            elif mid_val < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
+
+        # # time logm + logn
+        # # space 1
+        # m = len(matrix)
+        # n = len(matrix[0])
+        # l, r = 0, m-1
+        # while l <= r:
+        #     m = (l+r)//2
+        #     if target >= matrix[m][0] and target <= matrix[m][n-1]:
+        #         # binary search 
+        #         l, r, row = 0, n-1, m
+        #         while l <= r:
+        #             m = (l+r)//2
+        #             if target == matrix[row][m]:
+        #                 return True
+        #             elif target < matrix[row][m]:
+        #                 r = m - 1
+        #             else:
+        #                 l = m + 1
+        #         break
+
+        #     elif target < matrix[m][0]:
+        #         r = m - 1
+        #     else:
+        #         l = m + 1
+    
+        # return False
+
+        # # loops
+        # # time mn
+        # # space 1
+        # m = len(matrix)
+        # n = len(matrix[0])
+
+        # for i in range(m):
+        #     if target <= matrix[i][n-1]:
+        #         for j in range(n):
+        #             if target == matrix[i][j]:
+        #                 return True
+
+        # return False
+
         '''
         Binary Search
         Time: O(m + log(n))
