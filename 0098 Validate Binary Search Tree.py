591b7ff4f7bb4623a20c4fa655dc7c628c26a06f
Suraj Khamkar <112414255+khamkarsuraj@users.noreply.github.com>
2025-01-31 23:33:52 -0500

    DFS

--- 0098 Validate Binary Search Tree.py
+++ 0098 Validate Binary Search Tree.py
@@ -5,17 +5,37 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # Recursion
-    # Time: O(n) n is no of nodes
-    # Space: O(h) h is height of tree
-    def validate(self, node, lower, upper):
-        if not node:
-            return True
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        # understand why we pass min_val and max_val on line 23 (imp line)
+        # dfs
+        # time n
+        # space h
+        def dfs(min_val, root, max_val):
+            if not root:
+                return True
+        
+            if not root.val > min_val:
+                return False
+            
+            if not root.val < max_val:
+                return False
 
-        if node.val > lower and node.val < upper:
-            return self.validate(node.left, lower, node.val) and self.validate(node.right, node.val, upper)
+            return dfs(min_val, root.left, root.val) and dfs(root.val, root.right, max_val) #imp line
+        
+        return dfs(float('-inf'), root, float('inf'))
+    
+    # # Recursion
+    # # Time: O(n) n is no of nodes
+    # # Space: O(h) h is height of tree
+    # def validate(self, node, lower, upper):
+    #     if not node:
+    #         return True
 
-        return False
+    #     if node.val > lower and node.val < upper:
+    #         return self.validate(node.left, lower, node.val) and self.validate(node.right, node.val, upper)
 
-    def isValidBST(self, root: TreeNode) -> bool:
-        return self.validate(root, float("-inf"), float("+inf"))
+    #     return False
+
+    # def isValidBST(self, root: TreeNode) -> bool:
+    #     return self.validate(root, float("-inf"), float("+inf"))
+    
